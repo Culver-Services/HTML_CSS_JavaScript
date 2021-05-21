6e9790eac100a0d5339f665e78a1262f5af51556
Chadwick <83472455+productivechad@users.noreply.github.com>
2021-05-21 17:22:07 -0400

    Add files via upload

---
+++ Basic_JavaScript_1.js
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+<html>
+<body>
+	<button onclick="My_First_Function()">
+	Click me!
+	</button>
+	<p id="Irish"></p>
+<script>
+	function My_First_Function() {
+		var String ="Kiss me, I'm Irish!";
+		var ressult = String.fontcolor("green");
+		document.getElementId("Irish).innerHTML =
+		result;
+		}
+</script>
+</body>
+</html>
